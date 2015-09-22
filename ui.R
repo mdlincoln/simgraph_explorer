2dd3603b789dd1e89ac82ad3ded4debd8cec8eba
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-09-22 14:57:33 -0400

    more interesting defaults

--- ui.R
+++ ui.R
@@ -3,10 +3,10 @@ library(shinydashboard)
 
 graph_plot <- plotOutput("graph_plot")
 
-nodes <- textInput("nodes", "Number of nodes", value = 30)
-pa.exp <- textInput("pa.exp", "Attachment exponent", value = 2)
-aging.exp <- textInput("aging.exp", "Aging exponent", value = 2)
-m <- textInput("m", "Edges added each time", value = 1)
+nodes <- textInput("nodes", "Number of nodes", value = 100)
+pa.exp <- textInput("pa.exp", "Attachment exponent", value = 0.5)
+aging.exp <- textInput("aging.exp", "Aging exponent", value = 0.7)
+m <- textInput("m", "Edges added each time", value = 2)
 directed <- checkboxInput("directed", "Directed?", value = FALSE)
 
 fluidPage(
