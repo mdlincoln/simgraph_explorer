cfbbf6a0a3bf3e0562770ae6b18a3fc2cc9e9d3c
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-09-23 10:24:54 -0400

    Negative exponent means that nodes die...

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ graph_plot <- plotOutput("graph_plot")
 
 nodes <- textInput("nodes", "Number of nodes", value = 100)
 pa.exp <- textInput("pa.exp", "Attachment exponent", value = 0.5)
-aging.exp <- textInput("aging.exp", "Aging exponent", value = 0.7)
+aging.exp <- textInput("aging.exp", "Aging exponent", value = -3)
 m <- textInput("m", "Edges added each time", value = 2)
 directed <- checkboxInput("directed", "Directed?", value = FALSE)
 
