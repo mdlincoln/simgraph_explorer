bd66405f6c1c1b475ee7ea6007b4da14ddedbd03
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2015-09-23 10:25:22 -0400

    Bigger starting graph

--- ui.R
+++ ui.R
@@ -3,8 +3,8 @@ library(shinydashboard)
 
 graph_plot <- plotOutput("graph_plot")
 
-nodes <- textInput("nodes", "Number of nodes", value = 100)
 pa.exp <- textInput("pa.exp", "Attachment exponent", value = 0.5)
+nodes <- textInput("nodes", "Number of nodes", value = 300)
 aging.exp <- textInput("aging.exp", "Aging exponent", value = -3)
 m <- textInput("m", "Edges added each time", value = 2)
 directed <- checkboxInput("directed", "Directed?", value = FALSE)
